09339deb268158eb5ac4e69307001e63baa169a3
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-11-30 13:56:52 +0900

    20231130 - 오전 수업 gettersetter, 롬복

---
+++ 02.서블릿&JSP/exam02/src/main/java/exam02/Ex01.java
@@ -0,0 +1,6 @@
+package exam02;
+
+public class Ex01 {
+    // 객체 생성
+    Student s1 = new Student(1000, "이름1", "과목1")
+}
---
+++ 02.서블릿&JSP/exam02/src/main/java/exam02/Ex02.java
@@ -0,0 +1,13 @@
+package exam02;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        add(10,20);
+    }
+
+    public static int add(int num1, int num2){
+        int result = num1 + num2;
+
+        return result;
+    }
+}
