ccfc94b3b2af0a526d7e84ceb32af9dbd2865956
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-04 17:19:31 +0900

    231204 8교시 정리

--- 02.서블릿&JSP/exam04/src/main/java/filters/CommonFilter.java
+++ 02.서블릿&JSP/exam04/src/main/java/filters/CommonFilter.java
@@ -9,8 +9,13 @@ public class CommonFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        // 다음 필더가 있는지 확인하는 메서드 : 꼭 필요함
+        // 다음 필터가 있는지 호출하는 메서드 : 꼭 필요함
+        // 상위클래스가 하위클래스에 값으로 들러갈떄는 다입을 확인해야함(형변환필요)
         chain.doFilter(new CommonRequestWrapper((HttpServletRequest) request), response);
+
+        // 하위클래스HttpServletRequest 상위클래스request;
+        //HttpServletRequest req = (HttpServletRequest)request;
+
 
     }
 }
--- 02.서블릿&JSP/exam04/src/main/java/filters/FilterEx1.java
+++ 02.서블릿&JSP/exam04/src/main/java/filters/FilterEx1.java
@@ -9,7 +9,7 @@ public class FilterEx1 implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         System.out.println("FilterEx1 - 요청전");
 
-        // 다음 필더가 있는지 확인하는 메서드 : 꼭 필요
+        // 다음 필터가 있는지 호출하는 메서드 : 꼭 필요함
         chain.doFilter(request,response);
 
         System.out.println("FilterEx1 - 응답후");
