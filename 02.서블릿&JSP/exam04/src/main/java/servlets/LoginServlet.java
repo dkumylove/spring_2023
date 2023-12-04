4fd85ee0f2fd346da2fd56eb24b4909e04259554
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-04 12:04:01 +0900

    231204 3교시 - 내장객체 9가지, init()

--- 02.서블릿&JSP/exam04/src/main/java/servlets/LoginServlet.java
+++ 02.서블릿&JSP/exam04/src/main/java/servlets/LoginServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,10 +9,25 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/member/login")
+//@WebServlet("/member/login")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("로그인페이지");
+        System.out.println("doGet()");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        // 최초에 한번 호출됨. 서블릿 객체의 초기작업 정의(설정작업)
+        System.out.println("init()");
+        String key1 = config.getInitParameter("key1");
+        String key2 = config.getInitParameter("key2");
+        System.out.printf("key1=%s, key2=%s%n", key1, key2);
+    }
+
+    @Override
+    public void destroy() {
+        // 자원 해제작업을 주로 정의
+        System.out.println("destroy()");
     }
 }
--- 02.서블릿&JSP/exam04/src/main/webapp/jspex/ex02.jsp
+++ 02.서블릿&JSP/exam04/src/main/webapp/jspex/ex02.jsp
@@ -1,5 +1,12 @@
 <%@ page contentType="text/html; charset=UTF-8"%>
 
 <%
-    response.sendRedirect("ex03.jsp");
+    // 스클립틀릿
+    // _jspService 지역 내부
+    //response.sendRedirect("ex03.jsp");
+    out.write("<h1>ex02.jsp")
+%>
+
+<%!
+    // 선언식에서는 내장객체 접근 불가
 %>
