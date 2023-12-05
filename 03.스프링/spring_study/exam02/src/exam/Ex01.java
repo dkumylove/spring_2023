124a0b455c8209c3130aab8d5194fadd539cfdc5
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-05 14:25:27 +0900

    231205 4교시 - 객체(상속/ 생성자)

--- 03.스프링/spring_study/exam02/src/exam/Ex01.java
+++ 03.스프링/spring_study/exam02/src/exam/Ex01.java
@@ -4,7 +4,7 @@ public class Ex01 {
     public static void main(String[] args) {
         // 생성자 함수(메소드) -객체 생성하는 역할(힙 메모리에 필욯나 자원을 위한 공간 할당)
         C c = new C();
-
-        //int[] nums = new int[3];
+        A a = c;
+        B b = c;
     }
 }
