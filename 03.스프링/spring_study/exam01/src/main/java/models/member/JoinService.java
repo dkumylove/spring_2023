58421a8eb6b384bfaa2c70b85414c2d8f490465a
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-06 10:02:56 +0900

    231206 1교시 - 수업

--- 03.스프링/spring_study/exam01/src/main/java/models/member/JoinService.java
+++ 03.스프링/spring_study/exam01/src/main/java/models/member/JoinService.java
@@ -1,7 +1,21 @@
 package models.member;
 
 public class JoinService {
-    public void join(){
 
+    private MemberDao memberDao;
+    private JoinValidator validator;
+
+    // 생성자를 통한 주입
+    public JoinService(MemberDao memberDao, JoinValidator validator){
+        this.memberDao = memberDao;
+        this.validator = validator;
+    }
+
+    public void join(Member member){
+        // 데이터 검증
+        validator.validate(member);
+
+        // 데이터 추가
+        memberDao.register(member);
     }
 }
