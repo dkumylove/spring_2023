df00feabee16808dfcf4e42397ec015ae1372147
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-06 17:26:50 +0900

    231206 7교시 - 스프링수업

--- 03.스프링/spring_study/exam03/src/main/java/config/AppCtx3.java
+++ 03.스프링/spring_study/exam03/src/main/java/config/AppCtx3.java
@@ -1,8 +1,10 @@
 package config;
 
 import models.member.MemberDao;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.time.format.DateTimeFormatter;
 
@@ -10,11 +12,17 @@ import java.time.format.DateTimeFormatter;
 public class AppCtx3 {
 
     @Bean
+    //@Primary
     public MemberDao memberDao(){
         return new MemberDao();
     }
 
     @Bean
+    public MemberDao memberDao2(){
+        return new MemberDao();
+    }
+
+    //@Bean
     public DateTimeFormatter formatter(){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd.ha");
 
--- 03.스프링/spring_study/exam03/src/main/java/models/member/ListService.java
+++ 03.스프링/spring_study/exam03/src/main/java/models/member/ListService.java
@@ -1,6 +1,8 @@
 package models.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.lang.Nullable;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -9,23 +11,27 @@ public class ListService {
 
     private MemberDao memberDao;
 
-    private DateTimeFormatter formatter;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy.MM.dd");
+
+    //@Autowired(required = false)
     @Autowired
-    public void setFormatter(DateTimeFormatter formatter){
+    public void setFormatter(@Nullable DateTimeFormatter formatter){
         this.formatter = formatter;
     }
 
     @Autowired
     // setter를 통한 주입
-    public void setMemberDao(MemberDao memberDao){
+    public void setMemberDao(@Qualifier("memberDao") MemberDao memberDao){
         this.memberDao = memberDao;
     }
 
     public void print(){
         List<Member> members = memberDao.getList();
         for (Member member : members){
-            String regDtStr = formatter.format(member.getRegDt());
-            member.setRegDtStr(regDtStr);
+            if(formatter != null){
+                String regDtStr = formatter.format(member.getRegDt());
+                member.setRegDtStr(regDtStr);
+            }
             System.out.println(member);
         }
     }
