7354468b6c3127cb78d05cfcfed54acd61d8fa45
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-07 14:55:42 +0900

    231207 5교시 - 스프링 DI/ 의존자동주입/ 컴포넌트 스캔/ 빈 라이프 사이클

--- 03.스프링/spring_study/exam04/src/main/java/config/AppCtx2.java
+++ 03.스프링/spring_study/exam04/src/main/java/config/AppCtx2.java
@@ -1,14 +1,24 @@
 package config;
 
 import models.Message;
+import models.Message2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppCtx2 {
 
     @Bean
+    @Scope("prototype") // 객체가 생성될때마다
     public Message message(){
         return  new Message();
     }
+
+    // 인터페이스보다 @Bean을 더 많이 사용함. 매개변수가 있으면 안된다.
+    @Bean(initMethod = "init", destroyMethod = "close")
+    public Message2 message2() {
+        return new Message2();
+    }
+
 }
--- 03.스프링/spring_study/exam04/src/main/java/main/Ex02.java
+++ 03.스프링/spring_study/exam04/src/main/java/main/Ex02.java
@@ -1,6 +1,7 @@
 package main;
 
 import config.AppCtx2;
+import models.Message;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Ex02 {
@@ -8,7 +9,11 @@ public class Ex02 {
         // 객체 생성, 의존 설정, 초기화(afterPropertiesSet 호출)
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx2.class);
 
-        ctx.close(); // 컨테이너 소멸 -> 이전에 destroy() 호출
+        Message m1 = ctx.getBean(Message.class);
+        Message m2 = ctx.getBean(Message.class);
+        System.out.println(m1 == m2);
+
+        ctx.close(); // 컨테이너 소멸 -> 이 전에 destroy() 호출
 
     }
 }
