bdd98aed7a9709a489e58e230e9c8d22f2b27b40
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-07 17:45:06 +0900

    231207 자바보강 Annotation

---
+++ 99.자바보강/annotation/src/exam02/MyAnno2.java
@@ -0,0 +1,15 @@
+package exam02;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnno2 {
+    String value() default "기본값"; // 추가적인 정보전달 설정
+
+    int[] nums();
+}
