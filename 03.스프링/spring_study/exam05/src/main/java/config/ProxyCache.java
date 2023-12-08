0d7b6e4d70ad7c9bbf6cc16eef19df20df5b95a8
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-08 11:31:44 +0900

    231208 2교시 - 프록시(proxy)/ AOP

--- 03.스프링/spring_study/exam05/src/main/java/config/ProxyCache.java
+++ 03.스프링/spring_study/exam05/src/main/java/config/ProxyCache.java
@@ -4,14 +4,16 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Aspect
+@Order(1)
 public class ProxyCache {
 
-    private Map<Long, Long> cachDate = new HashMap<>();
+    private Map<Long, Object> cachDate = new HashMap<>();
 
     @Pointcut("execution(* AOP..*(long))")
     public void publicTarget() {}
@@ -22,10 +24,15 @@ public class ProxyCache {
         Object[] args = joinPoint.getArgs();  // 매개변수로 투입된 인자 값 (ex - 10L)
         Long num = (long)args[0];
         if(cachDate.containsKey(num)){
+            System.out.println("캐시값 사용");
             return cachDate.get(num);
         }
 
-        Object result = joinPoint.proceed();
+        Object result = joinPoint.proceed();  //
+
+        // 캐시저장
+        cachDate.put(num, result);
+        System.out.println("캐시저장");
 
         return result;
 
--- 03.스프링/spring_study/exam05/src/main/java/main/Ex01.java
+++ 03.스프링/spring_study/exam05/src/main/java/main/Ex01.java
@@ -8,13 +8,18 @@ public class Ex01 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
 
-        Calculator cal = ctx.getBean(Calculator.class);
+        Calculator cal = ctx.getBean(Calculator.class);     // Proxy 객체
         long result = cal.factorial(10L);
         System.out.printf("cal : %d%n", result);
 
-//        long result2 = cal.factorial(10L);
-//        long result3 = cal.factorial(10L);
-//        long result4 = cal.factorial(10L);
+        long result2 = cal.factorial(10L);
+        System.out.printf("cal : %d%n", result2);
+
+        long result3 = cal.factorial(10L);
+        System.out.printf("cal : %d%n", result3);
+
+        long result4 = cal.factorial(10L);
+        System.out.printf("cal : %d%n", result4);
 
         ctx.close();
 
