51d32743d94880e54ea47172bcc2d9432b7c2700
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-11 18:03:45 +0900

    231208 자바보강 지네릭스

--- 99.자바보강/generlcs/src/exam02/Box.java
+++ 99.자바보강/generlcs/src/exam02/Box.java
@@ -1,25 +1,28 @@
 package exam02;
 
 public class Box<T extends Fruit> {
-    private T item;     // Object
+    private T item; // Object
 
     //private static T item2;
-    //private  T[] item3= new T[];
+    //private T[] item3 = new T[];
 
-    public T getItem(){
+    public T getItem() {
         return item;
     }
-    
+
     public void setItem(T item) {
         this.item = item;
     }
-    
+
     public void printItem() {
-        // T -> Object, 객체를 만드는 시점 -> 투입된 타입으로 형변환
+        // T -> Object, 객체를 만드는 시점 -> 투입퇸 타입으로 형변환
         // T -> Fruit, 객체를 만드는 시점 -> 투입된 타입으로 형변환
         item.print();
     }
 
+    public <T> void testMethod(T t) { // 지네릭 메서드 - 메서드가 호출될떄 타입이 결정
+
+    }
 
     /**
      * T가 결정된 시점. 실제 컴파일될때 T 부분은 제거됨
--- 99.자바보강/generlcs/src/exam03/Juicier.java
+++ 99.자바보강/generlcs/src/exam03/Juicier.java
@@ -1,11 +1,17 @@
 package exam03;
 
+import exam02.Fruit;
+
 public class Juicier {
 
-    public static void make(FruitBox<?> appleBox) {
+    public static void make(FruitBox<? super Apple> fruitBox) { // Apple, Fruit, Object
         // 이부분을 확인하기 위해 와일드카드가 나옴 ?
+        System.out.println(fruitBox.getItems());
     }
 
+    public  static <T extends Fruit> void make2(FruitBox<T>  fruitBox) {
+        System.out.println(fruitBox.getItems());
+    }
 //    public static void make(FruitBox<Pear> perBox) {
 //
 //    }
