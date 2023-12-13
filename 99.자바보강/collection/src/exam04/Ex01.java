5f4f84bd873f2c66a67303af96fc649c01f78a8a
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-13 11:45:03 +0900

    231213 자바보강 3교시 - 컬랙션 Map

---
+++ 99.자바보강/collection/src/exam04/Ex01.java
@@ -0,0 +1,36 @@
+package exam04;
+
+import java.util.*;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        //HashMap<String, String> members = new HashMap<>();
+        // 키 자료형의 Commparable 인터페이스
+        // 대안 정렬 기준 java.util.Comparator 인터페이스
+        TreeMap<String, String> members = new TreeMap<>(Comparator.reverseOrder());
+        members.put("user01", "이름1");
+        members.put("user02", "이름2");
+        members.put("user03", "이름3");
+
+        // 수정
+        members.put("user02", "(수정)이름2");
+        //삭제
+        //members.remove("user02");
+
+        //String name = members.get("user02");
+        //System.out.println(name);
+
+        // 순서 쌍을 모두 가져옴
+        Set<Map.Entry<String, String>> entries = members.entrySet();
+
+        for(Map.Entry<String, String> entry : entries ){
+            System.out.printf("key = %s, value = %s%n", entry.getKey(), entry.getValue());
+        }
+
+        Set<String> keys = members.keySet();
+        Collection<String> values = members.values();
+
+        System.out.println("keys : " + keys);
+        System.out.println("values : " + values);
+    }
+}
