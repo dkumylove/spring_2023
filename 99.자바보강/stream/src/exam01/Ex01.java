f35367d7f4befb114a1fa6eaa26876c8d2e01ada
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-14 17:13:11 +0900

    231213 7 교시 - stream

---
+++ 99.자바보강/stream/src/exam01/Ex01.java
@@ -0,0 +1,15 @@
+package exam01;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Ex01 {
+    public static void main(String[] args) {
+        String[] words = {"aaa", "aa", "bbb", "ccc", "cc", "abc", "asdf"};
+        Arrays.stream(words).distinct().filter(s -> s.length() >=3).forEach(System.out::println);
+
+        List<String> words2 = Arrays.asList("aaa", "aa", "bbb", "ccc", "cc", "abc", "asdf");
+        words2.stream().distinct().filter(s -> s.length() >=3).forEach(System.out::println);
+    }
+}
