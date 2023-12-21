95abf33e7dd13325e0069dca71e9833bdf04d061
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-21 10:22:47 +0900

    231221 1교시 - 스프링부트 설정 및 파일업로드

---
+++ 04.스프링부트/exam01/src/main/java/com/choongang/configs/MvcConfig.java
@@ -0,0 +1,22 @@
+package com.choongang.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableConfigurationProperties(FileProperties.class)
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private FileProperties fileProperties;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 설정파일에 경로를 입력해 놓았기 때문에 fileProperties.getUrl(),fileProperties.getPath() 경로 호출
+        registry.addResourceHandler(fileProperties.getUrl() + "**")
+                .addResourceLocations("file:///" + fileProperties.getPath());
+    }
+}
