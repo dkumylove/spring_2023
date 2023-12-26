b5ce70df786fe0bc9df74b963d3221d3a6ed2d50
dkumylove <146877364+dkumylove@users.noreply.github.com>
2023-12-26 16:00:41 +0900

    231226 6교시 - Repository 설계하기

--- 04.스프링부트/exam02/src/main/java/org/choongang/repositories/BoardDataRepository.java
+++ 04.스프링부트/exam02/src/main/java/org/choongang/repositories/BoardDataRepository.java
@@ -1,7 +1,18 @@
 package org.choongang.repositories;
 
 import org.choongang.entities.BoardData;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardDataRepository extends JpaRepository<BoardData, Long> {
+
+    List<BoardData> findBySubjectContaining(String keyword);
+
+    // page 관련 포함되어있음
+    Page<BoardData> findBySubjectContaining(String keyword, Pageable pageable);
+
+    List<BoardData> findBySubjectContainingOrderBySeqDesc(String keyword);
 }
