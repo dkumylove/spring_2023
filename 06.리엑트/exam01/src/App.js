d2b09bcaab3efe46798d7972d500ad9cd975124e
dkumylove <146877364+dkumylove@users.noreply.github.com>
2024-02-07 12:19:14 +0900

    0207 : 리엑트수업 1,2,3, 교시

---
+++ 06.리엑트/exam01/src/App.js
@@ -0,0 +1,13 @@
+import MyComponent from "./components/MyComponent";
+
+const App = () => {
+  const name = '이이름';
+  return (
+    <MyComponent name={name} age={30}>
+      반갑습니다.
+    </MyComponent>
+  );
+  
+}
+
+export default App;
