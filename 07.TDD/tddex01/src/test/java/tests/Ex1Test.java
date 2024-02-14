afe527b084f5d1bd12b31ea81170e168ba240eb7
dkumylove <146877364+dkumylove@users.noreply.github.com>
2024-02-14 12:29:46 +0900

    TDD - 2.14 4교시 @TempDir

---
+++ 07.TDD/tddex01/src/test/java/tests/Ex1Test.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+
+public class Ex1Test {
+
+    @TempDir
+    private File tmpFile;
+
+    @Test
+    void test1() {
+        System.out.println(tmpFile.getAbsolutePath());
+    }
+
+    @Test
+    @Timeout(3)
+    void test2() {
+        try {
+            Thread.sleep(5000);  // 5초지연
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
