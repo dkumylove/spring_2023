b45ff66d095bb1cce63217a274f0fea1edc38711
dkumylove <dkumylove@gmail.com>
2024-03-12 21:06:45 +0900

    0208_5교시_라이프사이클

---
+++ 06.리엑트/exam01/src/components/LifeCycle.js
@@ -0,0 +1,70 @@
+import { Component } from 'react';
+
+class LifeCycle extends Component {
+  state = {
+    number: 0,
+    color: 'black',
+  };
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.color === 'orange') {
+      return { color: nextProps.color };
+    }
+
+    return null;
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  /*
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.number % 2 === 1) {
+      return false;
+    }
+
+    return true;
+  }
+  */
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    return 'snapshot';
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('componentDidUpdate');
+    console.log('snapshot', snapshot);
+    console.log('prevState', prevState);
+  }
+
+  render() {
+    console.log('render');
+    const result = false;
+    if (!result) {
+      throw new Error('에러 발생!!!!');
+    }
+
+    const { number, color } = this.state;
+    return (
+      <>
+        <h1>{number}</h1>
+        <h2>{color}</h2>
+        <button
+          type="button"
+          onClick={() => this.setState({ number: number - 1, color })}
+        >
+          -1
+        </button>
+        <button
+          type="button"
+          onClick={() => this.setState({ number: number + 1, color })}
+        >
+          +1
+        </button>
+      </>
+    );
+  }
+}
+
+export default LifeCycle;
